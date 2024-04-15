530abab2171a26a055f0dbb5d3b31d99b562cca3
prosper williams <wprosper92@gmail.com>
2024-04-15 13:34:32 +0100

    still building

--- src/assets/Pages/About/About.jsx
+++ src/assets/Pages/About/About.jsx
@@ -10,7 +10,7 @@ const page = () => {
          <p className="sora text-white text-center pt-4 pb-1">About Us</p>
          <div className="flex justify-center">
             {" "}
-            <h1 className=" text-4xl font-extrabold leading-none tracking-normal text-white md:text-5xl md:tracking-tight overflow-hidden pb-4 text-left max-w-[50rem] text-center">
+            <h1 className=" text-4xl font-extrabold leading-none tracking-normal text-white md:text-5xl md:tracking-tight overflow-hidden pb-4 max-w-[50rem] text-center">
                <span className=" text-transparent text-center  bg-clip-text leading-12 bg-gradient-to-r from-[#FF4A00] to-[#ef9e2c] lg:inline px-4">
                   our vision is to become trusted partners for businesses worldwide
                </span>
@@ -23,6 +23,25 @@ const page = () => {
                where brands thrive, communities flourish, and connections are made meaningful
             </p>
          </div>
+
+         <section className="flex justify-center mt-6 w-full">
+            <div className="grid grid-cols-1 justify-center border md:grid-cols-2 max-w-[80%] w-[90%]">
+               <div className="border">
+                  <p className="grotesk text-xs">
+                     HRLink was established in 2009 as a U.S.-based software development & consulting company with development centers in Vietnam. <br />
+                     Besides providing outsourcing services, HRLink has built and successfully launched its own software companies. The most notable companies with millions of
+                     users worldwide. <br />
+                     <br />
+                      HRLink is reliable, secure, and backed by a team of HR experts with a proven track record of developing high-quality technology solutions. <br />
+                      <br />
+                     With the growing size of many startups and SME companies and the increasing complexity of HR regulations and employee expectations, manual HR processes can
+                     become time-consuming, error-prone, and difficult to manage. HR management software provides a centralized platform for storing and automating HR processes to
+                     offer more free time for HR staff.
+                  </p>
+               </div>
+               <div className="border">hi</div>
+            </div>
+         </section>
          {/* <h1 className=" text-4xl font-extrabold leading-none tracking-normal text-white md:text-5xl md:tracking-tight overflow-hidden py-6 text-center">
             <span className="py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-[#FF4A00] to-[#ef9e2c] lg:inline">About us</span>
             <BackgroundCircles2 />
