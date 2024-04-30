07fe26eb5183ff899753b66f0462dd8a91e2a6e5
prosper williams <wprosper92@gmail.com>
2024-04-30 14:17:04 +0100

    making corrections

--- src/assets/components/TheTeam.jsx
+++ src/assets/components/TheTeam.jsx
@@ -31,12 +31,12 @@ const TheTeam = () => {
                      key={person.id}
                      className="relative z-1 max-w-[25rem] mx-auto sm:w-auto margin-auto items-center justify-center md:max-w-[100%]  rounded-lg overflow-hidden pb-6 bg-[#101010] shadow-[0px_4px_16px_rgba(68,68,68,0.1),_0px_8px_24px_rgba(68,68,68,0.1),_0px_16px_56px_rgba(68,68,68,0.1)]"
                   >
-                     <div className=" h-[40vh] md:h-[33vh] w-[100%] rounded-t-lg">
+                     <div className=" h-[35vh] md:h-[33vh] w-[100%] rounded-t-lg">
                         <img src={person.imgsrc} alt={person.name} className="w-full h-full object-cover object-center" />
                      </div>
                      <div className="pl-4">
                         <p className=" grotesk text-sm text-[#fe6700] py-1 font-bold ">{person.name}</p>
-                        <p className=" sora text-xs font-bold">{person.role}</p>
+                        <p className=" sora text-xs font-bold pb-1">{person.role}</p>
                      </div>
 
                      <div className="pl-4">
